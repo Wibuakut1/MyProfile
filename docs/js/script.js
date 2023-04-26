660ed17d14676f5ea49f8b753575224c301317a1
Athif Rahmawan <123344700+Athif5025@users.noreply.github.com>
2023-04-26 14:57:08 +0700

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -34,3 +34,12 @@ function changeImage() {
        setTheme('theme-light');
    }
 })();
+
+function openSideNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
